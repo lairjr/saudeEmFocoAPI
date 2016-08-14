4c6e8d653d3dca28fee66bc3945d1776d0f1d3fb
Lair Junior <lairjr@outlook.com>
2016-08-13 23:10:45 -0300

    Clean up spike

--- src/app.js
+++ src/app.js
@@ -1,34 +1,12 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import homeController from './controllers/home';
+import mongoose from 'mongoose';
 
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI);
 
-const occurrencesSchema = new mongoose.Schema({
-  title: { type: String, required: true }
-});
-
-const occurrence = mongoose.model('occurrences', occurrencesSchema);
-
-const saveOccurrences = (req, res) => {
-  const occur = new occurrence({ title: 'Teste 2' });
-
-  const savePromise = occur.save();
-
-  savePromise.then(
-    (o) => {
-      res.send('Saved!');
-    },
-    (e) => {
-      res.send(500);
-    }
-  );
-};
-
 app.get('/', homeController.get);
-app.get('/occurrences', saveOccurrences);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Saude em Foco API running at port 5000...');
