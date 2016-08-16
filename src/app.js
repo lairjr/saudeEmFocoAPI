d1201b03c0fd4ca8b30f158dc6dd5b55336e2d8f
Lair Junior <lairjr@outlook.com>
2016-08-16 01:27:22 -0300

    Add occurrences endpoint

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import homeController from './controllers/home';
 import mongoose from 'mongoose';
+import occurrencesRouter from './routes/occurrences';
 
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI);
 
 app.get('/', homeController.get);
+app.use('/occurrences', occurrencesRouter);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Saude em Foco API running at port 5000...');
 });
+
+export default app;
