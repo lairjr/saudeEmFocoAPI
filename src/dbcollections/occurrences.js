ded8a764322ee0bbc86c8e08b29b8327925bdb12
Lair Junior <lairjr@outlook.com>
2016-08-17 23:44:55 -0300

    Add location field on occurrence schema

--- src/dbcollections/occurrences.js
+++ src/dbcollections/occurrences.js
@@ -2,7 +2,14 @@ import mongoose from 'mongoose';
 
 const occurrencesSchema = new mongoose.Schema({
   description: { type: String, required: true },
-  status: { type: String, required: true }
+  status: { type: String, required: true },
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [Number]
+  }
 });
 
 export default mongoose.model('occurrences', occurrencesSchema);
