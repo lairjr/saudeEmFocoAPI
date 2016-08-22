91b91ead02dcd0ca99633de909cb326e3085db1b
Lair Junior <lairjr@outlook.com>
2016-08-22 00:57:33 -0300

    Add cors enable on api

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ const app = express();
 mongoose.connect(process.env.MONGODB_URI);
 
 app.get('/', homeController.get);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(bodyParser.json());
 app.use('/occurrences', occurrencesRouter);
 
