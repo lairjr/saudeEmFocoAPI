e5571e88dbb3d0db5fb936fb16f29e58ad242ff3
Lair Junior <lairjr@outlook.com>
2016-11-21 10:12:08 -0200

    Add type field on occurrence

--- src/dbcollections/occurrences.js
+++ src/dbcollections/occurrences.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const occurrencesSchema = new mongoose.Schema({
   description: { type: String, required: true },
   status: { type: String, required: true },
+  type: { type: String, required: true },
   location: {
     type: {
       type: String,
