b0562cc62bc77cbec407d3f8a49fbe564c934e05
Lair Junior <lairjr@outlook.com>
2016-11-27 18:55:41 -0200

    Change group by to serve side

--- src/controllers/cities.js
+++ src/controllers/cities.js
@@ -5,21 +5,33 @@ const citiesController = (dbModel) => {
 };
 
 const get = (dbModel) => (req, res) => {
-  dbModel.aggregate([
-    {
-      $project: {
-        city: 1
+  dbModel.find((e, results) => {
+    const data = results.reduce((acc, occurrence) => {
+      const cityData = acc.find((c) => (c.name === occurrence.city));
+
+      if (cityData) {
+        const newArray = acc.splice(acc.indexOf(cityData), 1);
+        return [
+          ...acc,
+          {
+            ...cityData,
+            numberOfOccurrences: cityData.numberOfOccurrences + 1,
+            numberOfReviews: (occurrence.status === 'REVIEW') ? cityData.numberOfReviews + 1 : cityData.numberOfReviews
+          }
+        ];
+      } else {
+        return [
+          ...acc,
+          {
+            name: occurrence.city,
+            numberOfOccurrences: 1,
+            numberOfReviews: (occurrence.status === 'REVIEW') ? 1 : 0
+          }
+        ];
       }
-    },
-    {
-      $group: {
-        _id: "$city",
-        numberOfOccurrences: { $sum: 1 }
-      }
-    }
-  ], (e, results) => {
-    console.log(e);
-    res.json(results);
+    }, []);
+
+    return res.json(data);
   });
 };
 
